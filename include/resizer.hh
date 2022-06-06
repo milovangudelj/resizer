4ee56211ab92029e1ba1c72e40fb0fdf31ea2b27
Milovn Gudelj <milovan.gudelj@gmail.com>
2022-06-06 11:55:18 +0200

    Removed unnecessary opencv modules

--- include/resizer.hh
+++ include/resizer.hh
@@ -6,9 +6,6 @@
 #include <string>
 
 #include <opencv2/core.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/videoio.hpp>
 
 namespace rr
 {
--- src/resizer.cc
+++ src/resizer.cc
@@ -3,9 +3,8 @@
 #include <string>
 
 #include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/videoio.hpp>
 
 #include "resizer.hh"
 
